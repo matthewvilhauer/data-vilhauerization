04abce511898dd6fec394b227ba2163b6d3b784d
Matthew Vilhauer <matthewvilhauer@gmail.com>
2017-01-29 17:16:34 -0800

    Sets up project with Express, Nodemon, and d3 libraries.

---
+++ server.js
@@ -0,0 +1,27 @@
+// SERVER-SIDE JAVASCRIPT
+
+//require express in our app
+var express = require('express'),
+    app = express(),
+    appPort = process.env.PORT || 3000;
+
+// serve static files from key folders
+app.use('/scripts', express.static('scripts'));
+app.use('/styles', express.static('styles'));
+app.use('/data', express.static('lib'));
+
+/**********
+ * ROUTES *
+ **********/
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(appPort, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
